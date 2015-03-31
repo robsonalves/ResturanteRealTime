52bcb5b88fb74b496cc1179deaca8fed4a7ce8fa
Robson Alves <robson.alves_desenv@hotmail.com>
2015-03-31 19:28:58 -0300

    Save the 'Informar Pedido' passing the information of order to kitche keeper

---
+++ Restaurante.Web/Js/Garcom/Garcom.js
@@ -0,0 +1,22 @@
+﻿
+var Garcom = Garcom || {};
+var pedido = pedido || {};
+
+Garcom.InformarCozinha = function () {
+    var self = this;
+    pedido = self.GetPedido();
+    appCozinha.server.informarCozinha(pedido);
+}
+
+
+Garcom.GetPedido  = function(__pedido) {
+    pedido = __pedido;
+}
+
+$("#btnSalvar").click(function() {
+    var form = $("#formPedido");
+    self.GetPedido(form.serialize());
+});
+    
+        
+    
