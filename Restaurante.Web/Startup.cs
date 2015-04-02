822cabc5ee54ffc24678473739df0c499ec3e3d9
Robson Alves <nosbor89@hotmail.com>
2015-04-02 10:16:39 -0300

    Implement oAtuh with Owin.Identity EF

--- Restaurante.Web/Startup.cs
+++ Restaurante.Web/Startup.cs
@@ -1,8 +1,15 @@
-﻿using Owin;
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Restaurante.Web.Models;
+
 
 namespace Restaurante.Web
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
