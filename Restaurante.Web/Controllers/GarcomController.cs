a2bcaaee537da4d7093f11af5281013e6ccd39c0
Robson Alves <nosbor89@hotmail.com>
2015-04-19 12:52:00 -0300

    Implmented asp.net api

---
+++ Restaurante.Web/Controllers/GarcomController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Restaurante.Dominio.Entidade;
+using Restaurante.Web.ViewModel;
+
+namespace Restaurante.Web.Controllers
+{
+    [Authorize]
+    public class GarcomController : BaseController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Restaurante RealTime";
+
+            var garcom = GetCurrentGarcom();
+
+            var viewmodel = new PedidoViewModel(new Pedido(garcom));
+
+            return View(viewmodel);
+        }
+    }
+}
